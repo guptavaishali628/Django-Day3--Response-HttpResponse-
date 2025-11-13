73f38827b5e3ad1a8ba81dc4c5d99cb39d748e1a
vaishali <guptavaishali628@gmail.com>
2025-11-13 19:29:37 +0530

    HttpResponse with routing

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def landing(req):
+    print("My first django project...")
+    return HttpResponse("<h1 style='color:red;'>Welcome</h1>")
